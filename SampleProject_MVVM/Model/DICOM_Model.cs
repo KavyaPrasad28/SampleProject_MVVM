9b41b6674999d37119c7a81443805523a567f255
Hari Prasad c <haripc525@gmail.com>
2022-02-28 15:17:26 +0530

    Add project files.

---
+++ SampleProject_MVVM/Model/DICOM_Model.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleProject_MVVM.Model
+{
+    public class DICOM_Model : IDataErrorInfo, INotifyPropertyChanged
+    {
+        private string password;
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+                NotifyPropertyChanged("Password");
+            }
+        }
+        public string Name { get; set; }
+        public string Status { get; set; }
+
+        #region INotifyPropertyChanged Members
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+        string IDataErrorInfo.Error
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        string IDataErrorInfo.this[string propertyName]
+        {
+            get
+            {
+                return GetValidationError(propertyName);
+            }
+        }
+
+        #region Validation
+
+        static readonly string[] ValidatedProperties =
+        {
+            "Password"
+        };
+
+        public bool IsValid
+        {
+            get
+            {
+                foreach (string property in ValidatedProperties)
+                {
+                    if (GetValidationError(property) != null)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        string GetValidationError(String propertyName)
+        {
+            string error = null;
+            switch (propertyName)
+            {
+                case "Password":
+                    error = ValidatePassword();
+                    break;
+            }
+            return error;
+        }
+        private string ValidatePassword()
+        {
+            if (Password == null)
+            {
+                return null;
+            }
+            if (Password!= "1234")
+            {
+                return "Incorrect Password.";
+            }
+            return null;
+        }
+        #endregion
+    }
+}
---
+++ SampleProject_MVVM/ViewModels/DICOM_ViewModel.cs
@@ -0,0 +1,79 @@
+﻿using SampleProject_MVVM.Commands;
+using SampleProject_MVVM.Model;
+using SampleProject_MVVM.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleProject_MVVM.ViewModels
+{
+    public class DICOM_ViewModel
+    {
+        public static MainWindow getObject;
+        public static PasswordView GetPassword { get; set; }
+        public DICOM_Model DICOM { get; set; }
+        public List<DICOM_Model> Nodes { get; set; }
+        public MyCommands AddNewCommand { get; set; }
+        public MyCommands UnlockCommand { get; set; }
+        public MyCommands CloseCommand { get; set; }
+        public MyCommands CancelCommand { get; set; }
+        public DICOM_ViewModel()
+        {
+            DICOM = new DICOM_Model();
+            Nodes = new List<DICOM_Model>();
+            DICOM.Name = "23";
+            DICOM.Status = "Error";
+            //DICOM.Password = "1234";
+            Nodes.Add(DICOM);
+            AddNewCommand = new MyCommands(OnAddNew, CanAddNew);
+            UnlockCommand = new MyCommands(OnUnlock, CanUnlock);
+            CloseCommand = new MyCommands(OnClose, CanClose);
+            CancelCommand = new MyCommands(OnCancel, CanCancel);
+            
+        }
+
+        private bool CanCancel()
+        {
+            return true;
+        }
+
+        private void OnCancel()
+        {
+            GetPassword.Close();
+        }
+
+        private bool CanClose()
+        {
+            return true;
+        }
+
+        private void OnClose()
+        {
+            getObject.Close();
+        }
+
+        private bool CanAddNew()
+        {
+            return true;
+        }
+        private void OnAddNew()
+        {
+            PasswordView passwordView = new PasswordView();
+            passwordView.Show();
+            GetPassword = passwordView;
+        }
+        private bool CanUnlock()
+        {
+            return true;
+        }
+        private void OnUnlock()
+        {
+            if (DICOM.Password == "1234")
+            {
+                OnClose();
+            }
+        }
+    }
+}
